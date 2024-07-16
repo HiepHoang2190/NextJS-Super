b77711b5a0acafa553aba21bc5a349576e66387c
unknown <hieph@lotusoutsourcing.com>
2024-07-16 10:44:45 +0700

    video_17 create file config and validate env

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next"
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
-
+import evnConfig from "../../config"
+console.log(evnConfig)
 const fontSans = FontSans({ 
   subsets: ["latin"] ,
   variable: "--font-sans",
