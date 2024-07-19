5814b9383c97e5c32a714e7b16ab8a13dd1d0d21
unknown <hieph@lotusoutsourcing.com>
2024-07-19 11:55:24 +0700

    video 33 : Auto logout when expired access token

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,28 +1,38 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
-const privatePaths = ['/manage']
-const unAuthPaths = ['/login']
-
-
+const privatePaths = ["/manage"]
+const unAuthPaths = ["/login"]
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   // pathname: manage/dashboard
-  const isAuth = Boolean(request.cookies.get('accessToken')?.value)
+ 
+  const accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value
+
   // Chưa đăng nhập thì không cho vào private path
-  if(privatePaths.some(path => pathname.startsWith(path)) && !isAuth) {
-      return NextResponse.redirect(new URL('/login', request.url))
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !refreshToken) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  // Trường hợp đăng nhập rồi , nhưng accessToken lại hết hạn
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !accessToken && refreshToken) {
+    const url = new URL("/logout", request.url)
+    url.searchParams.set(
+      "refreshToken",
+      refreshToken
+    );
+    return NextResponse.redirect(url)
   }
   // Đăng nhập vào rồi thì không cho vào login nữa
-  if(unAuthPaths.some(path => pathname.startsWith(path)) && isAuth) {
-    return NextResponse.redirect(new URL('/', request.url))
+  if (unAuthPaths.some((path) => pathname.startsWith(path)) && refreshToken) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
   return NextResponse.next()
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/manage/:path*', '/login','/']
-}
+  matcher: ["/manage/:path*", "/login", "/"],
+};
